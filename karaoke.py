c2ddc54b337db1f6e86349523b4f4e8af7042b16
anunezd <alejandro_nd93@hotmail.com>
2018-10-15 18:16:57 +0200

    v2 crea ficheros json

--- karaoke.py
+++ karaoke.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import json
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSMILHandler
@@ -18,14 +19,21 @@ class MiKaraoke:
 
     def __str__(self):
         exit = ""
-        for etiqueta in self.lista:
-            elemento = etiqueta[0]
-            atributo = etiqueta[1]
-            exit += '\n' + elemento + '\t'
+        for elemento in self.lista:
+            nombre = elemento[0]
+            atributo = elemento[1]
+            exit += '\n' + nombre + '\t'
             for valor in atributo:
                 if atributo[valor] != '':
                     exit += valor + '="' + atributo[valor] + '"' + '\t'
         return exit
+
+    def to_json(self, filesmil, filejson=''):
+        if filejson == '':
+            filejson = filesmil.replace('.smil', '.json')
+        with open (filejson, 'w') as jsonfile:
+            json.dump(self.lista, jsonfile, indent=3)
+
 
 if __name__ == "__main__":
 
@@ -35,3 +43,4 @@ if __name__ == "__main__":
         sys.exit('Usage python3 karaoke.p file.smil')
     karaoke = MiKaraoke(file)
     print(karaoke)
+    karaoke.to_json(file)

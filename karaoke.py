cb4defe9c18bdb07521518e9dd8bbed5b2f7ca7a
anunezd <alejandro_nd93@hotmail.com>
2018-10-16 18:25:57 +0200

    v2-actualizamos a descarga de ficheros

--- karaoke.py
+++ karaoke.py
@@ -3,6 +3,7 @@
 
 import sys
 import json
+import urllib.request
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSMILHandler
@@ -18,19 +19,29 @@ if __name__ == "__main__":
         parser.parse(open(file))
         lista = cHandler.get_tags()
 
-        exit = ""
+        salida = ""
         for elemento in lista:
             nombre = elemento[0]
-            atributo = elemento[1]
-            exit += '\n' + nombre + '\t'
-            for valor in atributo:
-                if atributo[valor] != '':
-                    exit += valor + '="' + atributo[valor] + '"' + '\t'
-        print (exit)
+            etiqueta = elemento[1]
+            salida += '\n' + nombre + '\t'
+            for atributo in etiqueta:
+                if etiqueta[atributo] != '':
+                    salida += atributo + '="' + etiqueta[atributo] + '"' + '\t'
+        print (salida)
 
         file = file.replace('.smil', '.json')
         with open (file, 'w') as jsonsalida:
             json.dump(lista, jsonsalida, indent=3)
+
+        for elemento in lista:
+            etiqueta = elemento [1]
+            for atributo in etiqueta:
+                if atributo == 'src':
+                    if etiqueta[atributo].startswith('http://'):
+                        url = etiqueta[atributo]
+                        file = url.split('/')[-1]
+                        urllib.request.urlretrieve(url,file)
+                        etiqueta[atributo] = file
 
     except IndexError:
         sys.exit('Usage python3 karaoke.p file.smil')

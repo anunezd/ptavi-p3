fb76564328f2ec1f1051fea9b4d4d285825581d5
anunezd <alejandro_nd93@hotmail.com>
2018-10-16 19:07:12 +0200

    v3-actualizamos a orientacion a objetos

--- karaoke.py
+++ karaoke.py
@@ -9,39 +9,53 @@ from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSMILHandler
 
 
-if __name__ == "__main__":
+class KaraokeLocal:
 
-    try:
-        file = sys.argv[1]
+    def __init__(self, file):
         parser = make_parser()
         cHandler = SmallSMILHandler()
         parser.setContentHandler(cHandler)
         parser.parse(open(file))
-        lista = cHandler.get_tags()
+        self.lista = cHandler.get_tags()
 
+    def __str__(self):
         salida = ""
-        for elemento in lista:
+        for elemento in self.lista:
             nombre = elemento[0]
             etiqueta = elemento[1]
             salida += '\n' + nombre + '\t'
             for atributo in etiqueta:
                 if etiqueta[atributo] != '':
                     salida += atributo + '="' + etiqueta[atributo] + '"' + '\t'
-        print (salida)
+        return (salida)
 
-        file = file.replace('.smil', '.json')
-        with open (file, 'w') as jsonsalida:
-            json.dump(lista, jsonsalida, indent=3)
+    def to_json(self, filesmil, filejson=''):
+        if filejson == '':
+            filejson = filesmil.replace('.smil', '.json')
+        with open (filejson, 'w') as jsonfile:
+            json.dump(self.lista, jsonfile, indent=3)
 
-        for elemento in lista:
+    def do_local(self):
+        for elemento in self.lista:
             etiqueta = elemento [1]
             for atributo in etiqueta:
                 if atributo == 'src':
                     if etiqueta[atributo].startswith('http://'):
                         url = etiqueta[atributo]
-                        file = url.split('/')[-1]
-                        urllib.request.urlretrieve(url,file)
-                        etiqueta[atributo] = file
+                        arch = url.split('/')[-1]
+                        urllib.request.urlretrieve(url, arch)
+                        etiqueta[atributo] = arch
 
+if __name__ == "__main__":
+
+    try:
+        file = sys.argv[1]
     except IndexError:
         sys.exit('Usage python3 karaoke.p file.smil')
+
+    karaoke = KaraokeLocal(file)
+    print(karaoke)
+    karaoke.to_json(file)
+    karaoke.do_local()
+    karaoke.to_json(file, 'local.json')
+    print(karaoke)
